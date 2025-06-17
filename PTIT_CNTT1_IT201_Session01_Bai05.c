81df27b8be48650907024285dbe2d920d72ebac4
khanhh89 <khanhabcabc0000@gmail.com>
2025-06-17 10:47:00 +0700

    Update PTIT_CNTT1_IT201_Session01_Bai05.c

--- PTIT_CNTT1_IT201_Session01_Bai05.c
+++ PTIT_CNTT1_IT201_Session01_Bai05.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) { // đổi chỗ
+int main() {
+    int arr[] = {100, 64, 78, 1, 5, 90, 200};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Mảng trước khi sắp xếp:\n");
+    print(arr, n);
+    sort(arr, n);
+    printf("Mảng sau khi sắp xếp:\n");
+    print(arr, n);
+    return 0;
+}
+
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
@@ -8,34 +19,25 @@ void swap(int *a, int *b) { // đổi chỗ
 
 void sort(int arr[], int n) {
     int i, j;
-    int swap;
-    for (int i = 0; i<n-1 ; i++) {
-        swap = 0;
-        for (int j; j <n-i-1; j++) {  //o(n^2)
-            if (arr[j] > arr[j+1]) {
-                swap(&arr[j], &arr[j+1]);
-                swap = 1;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;
             }
         }
-        if (swap = 0) {
+        if (swapped == 0) {
             break;
         }
     }
 }
 
-void print( int arr[], int size) {
+void print(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("\n");
+        printf("%d ", arr[i]); 
     }
+    printf("\n");
 }
-int main() {
-    int arr[] = {100, 64, 78,1 , 5, 90, 200};  //o(1)
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Mang truong khi sap xep: \n");
-    print(arr, n);
-    sort(arr, n);
-    print("Mang sau khi sap xep: \n");
-    print(arr, n);
-}
-
 
