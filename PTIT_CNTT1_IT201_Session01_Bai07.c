2173526a33278097e914c6b74609ae439b9a40ea
khanhh89 <khanhabcabc0000@gmail.com>
2025-06-17 11:09:43 +0700

    ss1

--- PTIT_CNTT1_IT201_Session01_Bai07.c
+++ PTIT_CNTT1_IT201_Session01_Bai07.c
@@ -31,7 +31,7 @@ int main(){
 int bruteForce(int arr[], int n){
     for (int i = 0; i < n-1 ; i++) {
         for(int j = i+1; j<n; j++){
-            if(arr[i] >arr[j]){
+            if(arr[i] == arr[j]){
                 return 1;  //tim thay
             }
         }
